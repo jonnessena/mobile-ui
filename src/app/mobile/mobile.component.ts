6048f51d4dab3421f204f992cdc32b4109abf1c9
Jonnes Sena <jonnes-sena@hotmail.com>
2018-02-25 13:10:00 -0300

    Criação de módulo do formulário de cadastro de mobiles.

---
+++ src/app/mobile/mobile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'mobile-create',
+    templateUrl: 'mobile.component.html',
+    styleUrls: ['mobile.component.scss']
+})
+export class MobileCreateComponent{
+    
+}
---
+++ src/app/mobile/mobile.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { MobileCreateComponent } from "./mobile.component";
+
+@NgModule({
+    imports: [],
+    exports: [MobileCreateComponent],
+    declarations: [MobileCreateComponent]
+})
+export class MobileModule {}
